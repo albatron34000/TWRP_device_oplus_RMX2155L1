e7c478cad4a15c0a2a36f0943769d46d6d561847
Stim <153687700+StimLuks87@users.noreply.github.com>
2024-04-16 01:02:32 +0500

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -16,6 +16,6 @@
 ##************************************************************************##
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),RMX2155)
+ifeq ($(TARGET_DEVICE),RMX2155L1)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
